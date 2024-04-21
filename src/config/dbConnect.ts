7d1c3c1903029bbe6f42ef37183c4b5a8c95c9fb
Sahil <sahilaroraji2002@gmail.com>
2024-04-21 23:50:04 +0530

    Database Setup Done

---
+++ src/config/dbConnect.ts
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useUnifiedTopology: true, // Configurations for mongodb
+      useNewUrlParser: true,
+      autoIndex: true,
+    });
+    console.log("DB connected successfully!");
+  } catch (error: any) {
+    console.log(`Error:${error.message}`);
+  }
+};
+
+module.exports = dbConnect;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 const express = require("express");
+const dotenv = require('dotenv')
+const db = require('./config/dbConnect')
 const app = express();
 const cors = require("cors");
 const PORT = 8001;
+
+dotenv.config();
+db();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
