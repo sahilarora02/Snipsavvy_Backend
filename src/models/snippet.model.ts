0d1f97f8df9ce8f8345054c4dfdc054130ccf059
Sahil <sahilrohera10@gmail.com>
2024-04-22 00:38:54 +0530

    [FEAT] created - workspace & snippet model

---
+++ src/models/snippet.model.ts
@@ -0,0 +1,51 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IComment extends Document {
+    user_id: string,
+    text: string
+}
+
+const commentSchema = new Schema<IComment>({
+    user_id: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    }
+});
+
+const snippetSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    code: {
+        type: String, //[FIXME] - checkout for better datatype for storing code, that keeps the indentation, spacing and formatting intact.
+        required: true
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    category_id: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    share_id: {
+        type: String,
+        required: true
+    },
+    comments: {
+        type: [commentSchema], // Array of comments
+        default: []
+    }
+});
+
+const Snippet = mongoose.model("Snippet", snippetSchema);
+export default Snippet
